a309f540d0540d4431f5af092b08231a48a3317d
houzhenggangGit <hou.zg@foxmail.com>
2018-12-12 16:16:25 +0800

    多线程

---
+++ src/main/java/taskExecutor/AsyncTaskService.java
@@ -0,0 +1,19 @@
+package taskExecutor;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AsyncTaskService
+{
+    @Async
+    public void executeAsyncTask(int i)
+    {
+        System.out.println("执行异步任务:"+i);
+    }
+    @Async
+    public void executeAsyncTaskPlus(int i)
+    {
+        System.out.println("执行异步任务"+i+"+1:"+(i+1));
+    }
+}
